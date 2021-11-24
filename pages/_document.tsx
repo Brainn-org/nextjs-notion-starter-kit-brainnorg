e42fcacba7a3c10428b53509219edc6b4711ec88
lucascervera <lucas@lucascervera.com>
2021-11-24 13:38:47 +0100

    Google Tag Manager added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,14 @@ export default class MyDocument extends Document {
     return (
       <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
         <Html lang='en'>
-          <Head>               
+          <Head>
+            
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KT3D4DG');</script>
+            
             <link rel='shortcut icon' href='/favicon.png' />
 
             <link
@@ -38,6 +45,10 @@ export default class MyDocument extends Document {
           </Head>
 
           <body>
+
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KT3D4DG"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+            
             <script src='noflash.js' />
 
             <Main />
